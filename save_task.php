2799fb7af98bb20ea4b47a6105d24d2607339b41
Emanuel <emanuel_vera@live.com.ar>
2023-06-23 04:39:39 -0300

    Poyecto crud con php estilizado con bootstrap

---
+++ save_task.php
@@ -0,0 +1,18 @@
+<?php
+if(isset($_POST['save_task'])){
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+
+    $query = "INSERT INTO task(title, description) VALUES ('$title', '$description')";
+    $result = mysqli_query($conn, $query);
+
+    if(!$result){
+        die("Error: conexion no establecida");
+    }
+
+    $_SESSION['message'] = 'Task Saved succesfully';
+    $_SESSION['message_type'] = 'success';
+
+    header("Location: index.php");
+}
+?>
